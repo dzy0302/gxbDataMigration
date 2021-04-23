46aded45dbbcf6cdeaa1f845209db4826b75eb23
。。。源 <apple_dzy@163.com>
2021-04-23 09:58:53 +0800

    新增 心电图-QTc间期

--- main.py
+++ main.py
@@ -4036,6 +4036,15 @@ def transplant():
                 logging.error('[更新异常]' + str(ex))
                 conn.rollback()
                 break
+        if orgin[12] == 'QTc间期：':
+            try:
+                cur.execute(
+                    'UPDATE record_gxb2 SET qtcjq = %s WHERE ID = %s;', (orgin[14], table_gxb_id))
+                conn.commit()
+            except Exception as ex:
+                logging.error('[更新异常]' + str(ex))
+                conn.rollback()
+                break
 
 
 
