97af4d2e98e77f47bd246ce0b76cd8348aea09bd
。。。源 <apple_dzy@163.com>
2021-06-21 15:20:41 +0800

    新增 冠脉CTA-LAD近段

--- main.py
+++ main.py
@@ -7537,6 +7537,57 @@ def transplant():
                     conn.rollback()
                     if error_stop:
                         break
+        if orgin[12] == 'LAD:_近段:':
+            if orgin[14] == 'LAD:_近段:无':
+                try:
+                    cur.execute(
+                        'UPDATE record_gxb2 SET gmcta_ladjd = %s WHERE ID = %s;', (0, table_gxb_id))
+                    conn.commit()
+                except Exception as ex:
+                    logging.error('[更新异常]' + str(ex))
+                    conn.rollback()
+                    if error_stop:
+                        break
+            if orgin[14] == 'LAD:_近段:轻度狭窄（<50%）':
+                try:
+                    cur.execute(
+                        'UPDATE record_gxb2 SET gmcta_ladjd = %s WHERE ID = %s;', (2, table_gxb_id))
+                    conn.commit()
+                except Exception as ex:
+                    logging.error('[更新异常]' + str(ex))
+                    conn.rollback()
+                    if error_stop:
+                        break
+            if orgin[14] == 'LAD:_近段:中度狭窄（50%-75%）':
+                try:
+                    cur.execute(
+                        'UPDATE record_gxb2 SET gmcta_ladjd = %s WHERE ID = %s;', (3, table_gxb_id))
+                    conn.commit()
+                except Exception as ex:
+                    logging.error('[更新异常]' + str(ex))
+                    conn.rollback()
+                    if error_stop:
+                        break
+            if orgin[14] == 'LAD:_近段:重度狭窄（76%-99%）':
+                try:
+                    cur.execute(
+                        'UPDATE record_gxb2 SET gmcta_ladjd = %s WHERE ID = %s;', (4, table_gxb_id))
+                    conn.commit()
+                except Exception as ex:
+                    logging.error('[更新异常]' + str(ex))
+                    conn.rollback()
+                    if error_stop:
+                        break
+            if orgin[14] == 'LAD:_近段:重度狭窄（100%）':
+                try:
+                    cur.execute(
+                        'UPDATE record_gxb2 SET gmcta_ladjd = %s WHERE ID = %s;', (5, table_gxb_id))
+                    conn.commit()
+                except Exception as ex:
+                    logging.error('[更新异常]' + str(ex))
+                    conn.rollback()
+                    if error_stop:
+                        break
 
     cur.close()
     conn.close()
